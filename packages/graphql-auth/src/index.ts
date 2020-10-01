211098cf80587cb27d5711dc87eb4b38f6bb3b17
Felix Green <work@felix.green>
2020-10-01 15:00:35 -0400

    remove logs

--- packages/graphql-auth/src/index.ts
+++ packages/graphql-auth/src/index.ts
@@ -21,8 +21,6 @@ export const createAuthorization: CreateAuthorization = ({ hasPermission }) => {
     callback: Function
   ) => {
     return async (root: any, params: any, context: any, info: any) => {
-      console.warn('inside authozation function()');
-
       if (typeof roleOrConfig === 'string') {
         if (!hasPermission(roleOrConfig, context.user)) {
           throw new Error(`This user does not have the ${roleOrConfig} role`);
@@ -32,12 +30,8 @@ export const createAuthorization: CreateAuthorization = ({ hasPermission }) => {
       if (typeof roleOrConfig === 'object') {
         const { test, role, skip } = roleOrConfig;
 
-        console.warn('authArgs', { test, role, skip });
-
         if (typeof test === 'function') {
           const passedTest = test(root, params, context, info);
-
-          console.warn('passedTest?', { passedTest });
 
           if (!passedTest) {
             throw new Error('This user does not pass the authoerization test');
@@ -47,23 +41,18 @@ export const createAuthorization: CreateAuthorization = ({ hasPermission }) => {
         if (typeof skip === 'function') {
           const didSkip = skip(root, params, context, info);
 
-          console.warn('didSkip?', { didSkip });
-
           if (!didSkip) {
             throw new Error('This user does not skip the authorization check');
           }
         }
 
         if (typeof role === 'string') {
-          // TODO: check when we can set roles on an admin user
-
           if (!hasPermission(role, context.user)) {
             throw new Error(`This user does not have the ${roleOrConfig} role`);
           }
         }
       }
 
-      console.warn('callback', { callback });
       if (typeof callback === 'function') {
         return callback(root, params, context, info);
       }
